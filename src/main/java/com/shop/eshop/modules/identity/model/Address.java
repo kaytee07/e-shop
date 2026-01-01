0c4c907a86d2a0df5013243eb3709d802719b3e6
kaytee07 <kofi.taylor07@gmail.com>
2026-01-01 09:30:41 +0000

    feature: setting up database parameters for users and address

---
+++ src/main/java/com/shop/eshop/modules/identity/model/Address.java
@@ -0,0 +1,14 @@
+package com.shop.eshop.modules.identity.model;
+
+import java.util.UUID;
+
+public class Address {
+    private UUID id;
+    private UUID userId;
+    private String phoneNumber;
+    private String StreetName;
+    private String city;
+    private String zip;
+    private String country;
+    private String digitalAdress;
+}
