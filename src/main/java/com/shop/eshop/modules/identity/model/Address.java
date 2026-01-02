f637ccc6d786b5fecb7ac47b57efb4d83733cb2c
kaytee07 <kofi.taylor07@gmail.com>
2026-01-02 04:12:59 +0000

    feat: setting up database entities and validating fields in identity module.

--- src/main/java/com/shop/eshop/modules/identity/model/Address.java
+++ src/main/java/com/shop/eshop/modules/identity/model/Address.java
@@ -1,14 +1,45 @@
 package com.shop.eshop.modules.identity.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
 import java.util.UUID;
 
+@Entity
+@Data
 public class Address {
+    @Id
+    @GeneratedValue
     private UUID id;
-    private UUID userId;
+
+    @OneToOne
+    @NotNull
+    private User user;
+
+    @NotBlank
     private String phoneNumber;
+
+
+    @Size(max = 100)
     private String StreetName;
+
+    @NotBlank
+    @Size(max = 50)
     private String city;
+
+    @Size(max = 10)
     private String zip;
+
+    @NotBlank
+    @Size(max = 50)
     private String country;
-    private String digitalAdress;
+
+    @Size(max = 15)
+    private String digitalAddress;
 }
